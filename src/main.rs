466371ae3955813f47aada2e8e4270d585c806ef
H1rono <hronok66@gmail.com>
2022-12-26 13:25:35 +0900

    :heavy_plus_sign: Depends on sqlx etc.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use anyhow::Result;
+use sqlx::mysql::MySqlPool;
+
+#[derive(Debug, PartialEq, Eq)]
+struct User {
+    id: u32,
+    name: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let pool = MySqlPool::connect(&env::var("DATABASE_URL")?).await?;
+    sqlx::query!(r"INSERT INTO `users` (`name`) VALUES ('H1rono')")
+        .execute(&pool)
+        .await?;
+    let users = sqlx::query_as!(User, r"SELECT `id`, `name` FROM `users`")
+        .fetch_all(&pool)
+        .await?;
+    for user in users {
+        println!("{:?}", user);
+    }
+    Ok(())
 }
