199ea6898d64243b1807cf5f938938bb52249021
H1rono <hronok66@gmail.com>
2022-12-28 08:31:44 +0900

    :bug: Fix docker failing due to macros in sqlx

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::env;
 
 use anyhow::Result;
 use sqlx::mysql::MySqlPool;
+use sqlx::FromRow;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, FromRow)]
 struct User {
     id: u32,
     name: String,
@@ -12,14 +13,15 @@ struct User {
 #[tokio::main]
 async fn main() -> Result<()> {
     let pool = MySqlPool::connect(&env::var("DATABASE_URL")?).await?;
-    sqlx::query!(r"INSERT INTO `users` (`name`) VALUES ('H1rono')")
+    sqlx::query(r"INSERT INTO `users` (`name`) VALUES ('H1rono')")
         .execute(&pool)
         .await?;
-    let users = sqlx::query_as!(User, r"SELECT `id`, `name` FROM `users`")
+    let users = sqlx::query(r"SELECT `id`, `name` FROM `users`")
         .fetch_all(&pool)
         .await?;
+    let users = users.iter().map(User::from_row);
     for user in users {
-        println!("{:?}", user);
+        println!("{:?}", user?);
     }
     Ok(())
 }
