d657063f36b1f0d4206ca72d4b47c92cfb83ba12
H1rono <hronok66@gmail.com>
2023-02-05 04:42:51 +0900

    :sparkles: Split model operations into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+use anyhow::{Context, Result};
+
+use sqlx::FromRow;
+
+#[derive(Debug, PartialEq, Eq, FromRow)]
+pub struct User {
+    pub id: u32,
+    pub name: String,
+}
+
+pub async fn read_all_users(pool: &sqlx::MySqlPool) -> Result<Vec<User>> {
+    let res = sqlx::query("SELECT `id`, `name` FROM `users`")
+        .fetch_all(pool)
+        .await
+        .context("Failed to SELECT all users")?;
+    let users = res
+        .into_iter()
+        .map(|u| User::from_row(&u).with_context(|| format!("Failed to parse row {u:?}")))
+        .collect::<Result<Vec<User>>>()?;
+    Ok(users)
+}
+
+pub async fn create_user(pool: &sqlx::MySqlPool, name: &str) -> Result<User> {
+    let res = sqlx::query("INSERT `users` (`name`) VALUES (?)")
+        .bind(name)
+        .execute(pool)
+        .await
+        .with_context(|| format!("Failed to INSERT a new user with name='{name}"))?;
+    let user = User {
+        id: res.last_insert_id() as u32,
+        name: String::from(name),
+    };
+    Ok(user)
+}
+
+pub async fn update_user(pool: &sqlx::MySqlPool, id: u32, name: &str) -> Result<User> {
+    sqlx::query("UPDATE `users` SET `name` = ? WHERE `id` = ?")
+        .bind(name)
+        .bind(id)
+        .execute(pool)
+        .await
+        .with_context(|| format!("Failed to UPDATE user name='{name}' WHERE id='{id}'"))?;
+    let user = User {
+        id,
+        name: String::from(name),
+    };
+    Ok(user)
+}
+
+pub async fn delete_user(pool: &sqlx::MySqlPool, id: u32) -> Result<()> {
+    sqlx::query("DELETE FROM `users` WHERE `id` = ?")
+        .bind(id)
+        .execute(pool)
+        .await
+        .with_context(|| format!("Failed to DELETE user WHERE id={id}"))?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,14 @@ use std::env;
 
 use anyhow::Result;
 use sqlx::mysql::MySqlPool;
-use sqlx::FromRow;
 
-#[derive(Debug, PartialEq, Eq, FromRow)]
-struct User {
-    id: u32,
-    name: String,
-}
+use dbexample_rs::{create_user, read_all_users};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let pool = MySqlPool::connect(&env::var("DATABASE_URL")?).await?;
-    sqlx::query(r"INSERT INTO `users` (`name`) VALUES ('H1rono')")
-        .execute(&pool)
-        .await?;
-    let users = sqlx::query(r"SELECT `id`, `name` FROM `users`")
-        .fetch_all(&pool)
-        .await?;
-    let users = users.iter().map(User::from_row);
-    for user in users {
-        println!("{:?}", user?);
-    }
+    create_user(&pool, "H1rono").await?;
+    let users = read_all_users(&pool).await?;
+    println!("{users:?}");
     Ok(())
 }
